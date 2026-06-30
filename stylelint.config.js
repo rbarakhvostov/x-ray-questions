ec49667d9c4b7d519d7922d26c2cacf7f949a550
rbarakhvostov <rbarakhvostov@gmail.com>
2026-06-30 23:38:50 +0300

    add stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,12 @@
+export default {
+  extends: ['stylelint-config-standard'],
+  rules: {
+    'import-notation': null,
+    'font-family-name-quotes': null,
+    'media-feature-range-notation': null,
+    'color-hex-length': null,
+    'color-function-alias-notation': null,
+    'color-function-notation': null,
+    'alpha-value-notation': null,
+  },
+};
