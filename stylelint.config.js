66a527a1ff0e974b8cac655bf452c7e2dda617c3
rbarakhvostov <rbarakhvostov@gmail.com>
2026-07-05 16:35:41 +0300

    changes

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,12 +1,23 @@
 export default {
   extends: ['stylelint-config-standard'],
   rules: {
-    'import-notation': null,
-    'font-family-name-quotes': null,
-    'media-feature-range-notation': null,
-    'color-hex-length': null,
-    'color-function-alias-notation': null,
-    'color-function-notation': null,
-    'alpha-value-notation': null,
+    // Для CSS-модулей разрешаем любые имена классов
+    'selector-class-pattern': null,
+
+    // Разрешаем :global и :local
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global', 'local', 'export'],
+      },
+    ],
+
+    // Разрешаем композицию
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['value', 'each', 'mixin', 'include', 'define-mixin'],
+      },
+    ],
   },
 };
